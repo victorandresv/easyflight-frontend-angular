40e11a26fe8454d222188d5e82bb8848a08d70a8
Victor Vargas <victor.andresv@gmail.com>
2023-11-28 10:52:07 -0300

    Flight information model form

---
+++ src/app/models/flight-information-model.ts
@@ -0,0 +1,9 @@
+export interface FlightInformationModel {
+    origin: string;
+    destiny: string;
+    departureDate: string;
+    returnDate: string;
+    adults: number;
+    children: number;
+    infants: number;
+}
--- src/app/views/flight-information/flight-information.component.ts
+++ src/app/views/flight-information/flight-information.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlightInformationModel } from '../../models/flight-information-model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-flight-information',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './flight-information.component.html',
   styleUrl: './flight-information.component.css'
 })
 export class FlightInformationComponent {
+
+  public flightInformation: FlightInformationModel = {
+    origin: '',
+    destiny: '',
+    departureDate: '',
+    returnDate: '',
+    adults: 1,
+    children: 0,
+    infants: 0
+  };
+
+  public ngOnInit() {
+  }
 
 }
