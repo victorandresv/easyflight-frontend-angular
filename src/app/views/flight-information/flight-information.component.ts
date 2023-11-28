2eecc1882c672eeb431b17c0ba52506357198f92
Victor Vargas <victor.andresv@gmail.com>
2023-11-28 14:33:43 -0300

    selectors population ready

--- src/app/views/flight-information/flight-information.component.ts
+++ src/app/views/flight-information/flight-information.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { FlightInformationModel } from '../../models/flight-information-model';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-flight-information',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgFor],
   templateUrl: './flight-information.component.html',
   styleUrl: './flight-information.component.css'
 })
@@ -17,9 +17,26 @@ export class FlightInformationComponent {
     destiny: '',
     departureDate: '',
     adults: 1,
+    infants: 0,
+    children: 0,
   };
 
+  public adultsOptions: number[] = [];
+  public childrenOptions: number[] = [];
+  public infantsOptions: number[] = [];
+
   public ngOnInit() {
+    for (let i = 1; i <= 9; i++) {
+      if (i >= 2) {
+        this.adultsOptions.push(i);
+      }
+      this.childrenOptions.push(i);
+      this.infantsOptions.push(i);
+    }
+  }
+
+  public SearchAction() {
+    console.log(this.flightInformation)
   }
 
 }
