8303527003956cf47a16418e196bdfd927643e9c
Victor Vargas <victor.andresv@gmail.com>
2023-11-28 10:22:32 -0300

    Form flight data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FlightInformationComponent } from './views/flight-information/flight-information.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FlightInformationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'easyflight_frontend_angular';
+  
 }
---
+++ src/app/views/flight-information/flight-information.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-flight-information',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './flight-information.component.html',
+  styleUrl: './flight-information.component.css'
+})
+export class FlightInformationComponent {
+
+}
